2bf0281940a12c37c87acfdc7251e5345193b432
Matan Dessaur <matandessaur@m8ndoes.com>
2023-08-02 17:20:52 -0400

    Small tweaks: 08022023

--- src/App.js
+++ src/App.js
@@ -80,7 +80,7 @@ export default function App() {
 
 const ViewContainer = styled.div`
   width: 100%;
-  height: 100%;
+  height: 100svh;
   display: flex;
   flex-direction: column;
   align-items: center;
